16eaecea028c476c4918b5f0463886cef7bba701
xiongyao <18778331181@sina.cn>
2022-09-04 01:30:28 +0800

    简单优化数据库

--- apps/ecs/models.py
+++ apps/ecs/models.py
@@ -4,9 +4,14 @@ from django.contrib.auth.models import AbstractUser
 
 class Ecs(models.Model):
     user_id = models.IntegerField(default=0, verbose_name="用户id")
+    server_id = models.CharField(default="", verbose_name="目标机器的id,比如serverA serverB", max_length=32)
+    status = models.CharField(default="", verbose_name="创建的状态,0创建中1创建成功2创建失败", max_length=32)
     batch_id = models.CharField(default="", verbose_name="操作的批次号", max_length=32)
     code = models.CharField(default="", verbose_name="唯一标识", max_length=32)
     cpu = models.CharField(default="", verbose_name="cpu型号", max_length=32)
     memory = models.CharField(default="", verbose_name="内存大小", max_length=32)
     disk = models.CharField(default="", verbose_name="硬盘存储大小", max_length=32)
     original_specs = models.CharField(default="", verbose_name="原始上传配置的文件oss地址", max_length=256)
+    reason = models.CharField(default="", verbose_name="创建失败原因", max_length=256)
+    request_content = models.TextField(default="", verbose_name="调用创建虚拟机对象的请求原文")
+    response_content = models.TextField(default="", verbose_name="调用创建虚拟机对象的响应原文")
--- apps/ecs/tasks.py
+++ apps/ecs/tasks.py
@@ -13,14 +13,17 @@ def celery_create_ecs_task(batch_id, oss_path, user_id):
     :param user_id:
     :return:
     """
-    spec_data = read_spec_from_oss(oss_path)
-    for i in spec_data:
-        Ecs.objects.create(**{
-            "user_id": user_id,
-            "batch_id": batch_id,
-            "code": i.split(",")[0],
-            "cpu": i.split(",")[0],
-            "memory": i.split(",")[0],
-            "disk": i.split(",")[0],
-            "original_specs": oss_path
-        })
+    # 异步实际创建虚拟机api接口的伪代码，前端可根据返回的batch_id查询返回当前
+    # 资源对象创建的状态
+    pass
+    # spec_data = read_spec_from_oss(oss_path)
+    # for i in spec_data:
+    #     Ecs.objects.create(**{
+    #         "user_id": user_id,
+    #         "batch_id": batch_id,
+    #         "code": i.split(",")[0],
+    #         "cpu": i.split(",")[0],
+    #         "memory": i.split(",")[0],
+    #         "disk": i.split(",")[0],
+    #         "original_specs": oss_path
+    #     })
